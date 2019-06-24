4c894f9dd7f9413d77f078d70ee09b212b8eaa65
nitanshu_nik <nitanshu.nik@flender.ie>
2019-06-24 12:42:50 +0100

    - updated file path

--- lib/nua_pay/nuapay_api_url.rb
+++ lib/nua_pay/nuapay_api_url.rb
@@ -1,2 +1,2 @@
-path = File.expand_path('lib/nua_pay/nua_pay_api_urls.yml')
+path = File.join(File.dirname(File.expand_path(__FILE__)), 'lib/nua_pay/nua_pay_api_urls.yml')
 NUAPAY_API = YAML.load_file( path )
