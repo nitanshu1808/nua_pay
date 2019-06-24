8da4aff2b863837006863498712f0b7863853186
nitanshu_nik <nitanshu.nik@flender.ie>
2019-06-24 11:20:19 +0100

    - specifying files with name

--- lib/nua_pay.rb
+++ lib/nua_pay.rb
@@ -1,4 +1,10 @@
-require "nua_pay/*.rb"
+require "nua_pay/version"
+require "nua_pay/nuapay_api_url"
+require "nua_pay/scheme"
+require "nua_pay/account"
+require "nua_pay/base"
+require "nua_pay/direct_debit_info"
+require "nua_pay/mandate_info"
 
 module NuaPay
 end
